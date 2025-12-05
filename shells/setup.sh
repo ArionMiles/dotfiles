91b7e2d5fb943aa97e8e70e09fdc1d280a139865
Kanishk Singh <hey@kanishk.io>
2025-12-05 14:14:09 +0530

    Add zprofile

--- shells/setup.sh
+++ shells/setup.sh
@@ -3,11 +3,20 @@ DOTFILESDIR=~/dotfiles
 
 if [ -f ~/.zshrc ]; then
         echo "Creating .zshrc backup."
-        mv ~/.zshrc ~/.zshrc$(date +%F-%R).bak
+        mv ~/.zshrc ~/.zshrc-$(date +%F-%R).bak
 fi
+
+if [ -f ~/.zprofile ]; then
+        echo "Creating .zprofile backup."
+        mv ~/.zshrc ~/.zprofile-$(date +%F-%R).bak
+fi
+
 
 echo "Soft linking .zshrc to ${HOME} directory."
 ln -s -v ${DOTFILESDIR}/shells/.zshrc ${HOME}/
+
+echo "Soft linking .zprofile to ${HOME} directory."
+ln -s -v ${DOTFILESDIR}/shells/.zprofile ${HOME}/
 
 echo "Soft linking aliases to ${HOME}/.config/aliases"
 ln -s -v ${DOTFILESDIR}/shells/aliases ${HOME}/.config/aliases
