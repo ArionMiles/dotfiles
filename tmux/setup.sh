528d0d82bf101e754965ca41464412c5a904773f
Kanishk Singh <hey@kanishk.io>
2025-12-06 01:35:02 +0530

    Move tmux config to ~/.config
    Tmux >3.2 works with XDG_CONFIG_HOME

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 DOTFILESDIR=~/dotfiles
 
-if [ -d ~/.tmux/plugins/tpm ]; then
+if [ -d ~/.config/tmux/plugins/tpm ]; then
 	echo "Tmux plugin manager already exists"
 else
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 fi
 
-if [ -f ~/.tmux.conf ]; then
-        echo "Found existing ~/.tmux.conf. Backing it up..."
-        mv ~/.tmux.conf ~/.tmux.conf-$(date +%F-%R).bak
+if [ -f ~/.config/tmux.conf ]; then
+        echo "Found existing ~/.config/tmux.conf. Backing it up..."
+        mv ~/.config/tmux.conf ~/.config/tmux.conf-$(date +%F-%R).bak
 fi
 
-if [ -f ~/.tmux.remote.conf ]; then
-        echo "Found existing ~/.tmux.remote.conf. Backing it up..."
-        mv ~/.tmux.remote.conf ~/.tmux.remote.conf-$(date +%F-%R).bak
+if [ -f ~/.config/tmux.remote.conf ]; then
+        echo "Found existing ~/.config/tmux.remote.conf. Backing it up..."
+        mv ~/.config/tmux.remote.conf ~/.config/tmux.remote.conf-$(date +%F-%R).bak
 fi
 
 
-echo "Soft linking $DOTFILESDIR/tmux/.tmux.conf to ${HOME} directory."
-ln -s -v ${DOTFILESDIR}/tmux/.tmux.conf ${HOME}
+echo "Soft linking $DOTFILESDIR/tmux/tmux.conf to ${HOME}/.config/tmux directory."
+ln -s -v ${DOTFILESDIR}/tmux/tmux.conf ${HOME}/.config/tmux/
 
-echo "Soft linking $DOTFILESDIR/tmux/.tmux.remote.conf to ${HOME} directory."
-ln -s -v ${DOTFILESDIR}/tmux/.tmux.remote.conf ${HOME}
+echo "Soft linking $DOTFILESDIR/tmux/tmux.remote.conf to ${HOME}/.config/tmux directory."
+ln -s -v ${DOTFILESDIR}/tmux/tmux.remote.conf ${HOME}/.config/tmux
 
