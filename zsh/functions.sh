203de9535dc293e36c523e08bfe5f7f68d6dded4
Kanishk Singh <hey@kanishk.io>
2026-01-01 18:44:42 +0530

    Add pbpng and pbtxt functions

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -4,3 +4,12 @@
 function killp() {
     lsof -i :$1 | awk 'NR>1 {print $2}' | xargs kill -9
 }
+
+# Upload from your clipboard using pbpaste to x0.at and get back the URL and copy it into pbcopy
+function pbtxt() {
+    pbpaste | curl -F 'file=@-' https://x0.at/ | tee >(pbcopy)
+}
+
+function pbpng() {
+    pngpaste - | curl -F 'file=@-;filename=clipboard.png' https://x0.at/ | tee >(pbcopy)
+}
